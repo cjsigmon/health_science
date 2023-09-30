1ef217f51e3c7cc67830e580ec64effbc20fd850
Caleb Sigmon <cjsigmon@email.unc.edu>
2023-09-30 19:24:52 -0400

    ednpoint madness maybe fixed for now

--- R/api.R
+++ R/api.R
@@ -4,7 +4,7 @@
 #* @param msg The message to echo back.
 #* @get /echo
 function(msg=""){
-  list(msg = paste0("The message is: '", msg, "'"))
+  list(msg = paste0("The message is: ", msg, ""))
 }
 
 #* Plot out data from the iris dataset
--- index.js
+++ index.js
@@ -1,12 +1,28 @@
-const express = require("express")
-const app = express()
-const axios = require("axios")
+const express = require("express");
+const app = express();
+const axios = require("axios");
+const cors = require("cors"); // Import the cors middleware
 
-app.get('/', (req, res) => {
-  axios.get("http://127.0.0.1:8000/echo?msg=hellow")
-  .then(response => {
-    console.log(response);
-})
-})
+// Enable CORS for all routes
+app.use(cors());
 
-app.listen(3000)
+// Define a route that accepts parameters
+app.get('/:param', (req, res) => {
+  const { param } = req.params;
+
+  // Construct the URL for your external API with parameters
+  const apiUrl = `http://127.0.0.1:8000/echo?msg=${param}`;
+
+  axios.get(apiUrl)
+    .then(response => {
+      // Send the API response back to the client as JSON
+      res.send(response.data);
+    })
+    .catch(error => {
+      // Handle errors here
+      console.log(error);
+      res.status(500).json({ error: "Internal Server Error" });
+    });
+});
+
+app.listen(3000);
