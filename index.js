035b7b83a1528b98c8c97cea864f5487502f0cea
Caleb Sigmon <cjsigmon@email.unc.edu>
2023-09-30 14:33:56 -0400

    node js init

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("hello world")
+// const apiUrl = 'http://127.0.0.1:6880/echo?msg=hello';
+
+// fetch(apiUrl)
+//   .then(response => {
+//     if (!response.ok) {
+//       throw new Error('Network response was not ok');
+//     }
+//     return response.json(); // Assuming the response is in JSON format
+//   })
+//   .then(data => {
+//     console.log(data);
+//   })
+//   .catch(error => {
+//     console.error('There was a problem with the fetch operation:', error);
+//   });
+
+
+var http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('Hello World!');
+}).listen(8080); 
